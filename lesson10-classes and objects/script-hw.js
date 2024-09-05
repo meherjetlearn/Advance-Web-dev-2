1d426a1e0ebccb69cab6bb281de1efe14905ae89
meherjetlearn <125950646+meherjetlearn@users.noreply.github.com>
2024-09-05 15:21:18 +0530

    Add files via upload

---
+++ lesson10-classes and objects/script-hw.js
@@ -0,0 +1,70 @@
+class Movie {
+    constructor(title, director, year) {
+      this.title = title
+      this.director = director
+      this.year = year
+      }
+}
+
+const defaultMovies = [
+    {
+    title: 'Jurassic Park',
+    director: 'John Doe',
+    year: '1990'
+    },
+    {
+    title: 'The Dead Pool',
+    director: 'Mathew Albison',
+    year: '2014'
+    },
+    {
+    title: 'The Avengers',
+    director: 'Joss Whedon',
+    year: '2012'
+    }
+]
+class UI{
+
+static addMovieToList = (movie) => {
+    const list = document.getElementById('movie-list');
+    const row = document.createElement('tr')
+    row.innerHTML=`<td>${movie.title}</td><td>${movie.director}</td><td>${movie.year}</td> <input type="button" value="Delete" onclick="deleteRow(this)"></td>`
+    
+        list.appendChild(row)
+        }
+    static displayMovies=()=>{
+        defaultMovies.forEach(movie => UI.addMovieToList(movie))
+    }
+    
+    
+   
+}
+UI.displayMovies()
+
+function deleteRow(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("movie-list").deleteRow(i);
+  }
+
+document.querySelector('#movie-form').addEventListener('submit', addAMovie)
+
+    function addAMovie(e) {
+    // prevent actual submission
+    e.preventDefault()
+    // Get Form Values
+       const title =document.querySelector('#title').value;
+       const director =document.querySelector('#director').value;
+       const year =document.querySelector('#year').value;
+    
+    // Instantiate a new Book object
+    const movie = new Movie(title,director,year)
+    // Add object to UI
+      UI.addMovieToList(movie)
+    }
+
+   
+
+
+
+
+
---
+++ lesson8/Project/script-hw.js
@@ -0,0 +1,9 @@
+
+let changebg = () => {
+    document.body.style.backgroundImage ='url("./images/Travel.jpg")';
+    }
+    
+document.getElementById("btn").addEventListener("click", changebg);
+
+//    //cd.. ==> to to go back  [change directory]
+
